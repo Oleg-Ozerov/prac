1d656d80a6945e9d75b8fbcaf7a9857a3a4c7a9b
Oleg <mvp.westbrook4k@gmail.com>
2020-09-24 13:05:54 +0300

    Updates

---
+++ precache-manifest.7292fb6b0795548c7387be24cc8368a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835f78cfc67e9b436570a88ef4d8f2aa",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "6a40151eabde7a9e84d6",
+    "url": "/TodoApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d2d013e51fccceb726a",
+    "url": "/TodoApp/static/js/2.66e42ca1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.66e42ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a40151eabde7a9e84d6",
+    "url": "/TodoApp/static/js/main.468e92a5.chunk.js"
+  },
+  {
+    "revision": "6178001199e45a76af3f",
+    "url": "/TodoApp/static/js/runtime-main.579f7b68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
